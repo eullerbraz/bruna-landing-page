5856d6f96fa62ba046610914f7f0d1e0513a9e5a
Euller Braz <euller.braz99@gmail.com>
2024-09-06 15:46:08 -0300

    feat: Create header and hero section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -24,6 +24,15 @@ export default {
           900: '#121316',
         },
       },
+      animation: {
+        'infinite-scroll': 'infinite-scroll 25s linear infinite',
+      },
+      keyframes: {
+        'infinite-scroll': {
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(-100%)' },
+        },
+      },
     },
     fontFamily: {
       sans: ['Poppins', ...defaultTheme.fontFamily.sans],
