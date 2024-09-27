d500b22670333c5c3fc5a6c098a6c31cd9cf99ac
Euller Braz <euller.braz99@gmail.com>
2024-09-27 14:44:51 -0300

    build: Add vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,14 @@ import tailwind from '@astrojs/tailwind'
 
 import partytown from '@astrojs/partytown'
 
+import vercel from '@astrojs/vercel/serverless'
+
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
   integrations: [
     tailwind(),
     partytown({
